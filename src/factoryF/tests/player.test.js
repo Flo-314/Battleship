e70b52860348adc48121f5895145fdbf51fc7304
flo-314 <julianflo1916@gmail.com>
2021-11-13 20:07:38 -0300

    basic test of player

---
+++ src/factoryF/tests/player.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
+import GameboardFactory from '../gameboard';
+import ShipFactory from '../ship';
+import PlayerFactory from '../player';
+
+const ia = PlayerFactory('ia');
+const player = PlayerFactory('player');
+player.attack(3, 4);
+test('check if playerturn is always true', () => {
+  expect(player.turn).toBe(true);
+});
+
+test('check if player attack works', () => {
+  expect(ia.gameboard.GameboardFactory[32]).toBe('X');
+});
+
+/*
+
+    Players can take turns playing the game by attacking the enemy Gameboard.
+
+    The game is played against the computer, so make ‘computer’ players capable of making random plays.
+     The AI does not have to be smart, but it should know whether or not a given move is legal.
+     (i.e. it shouldn’t shoot the same coordinate twice).
+
+ */
